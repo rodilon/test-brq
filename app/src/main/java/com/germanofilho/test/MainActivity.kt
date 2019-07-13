2e74d31d8a3c2206b5035455ecf071655de68475
rodilon <rodilon@brq.com>
2019-07-12 21:58:11 -0300

    Altera modelo e implementa logica para mostrar qtd de peças por carro e qtd de carros por montadora

--- app/src/main/java/com/germanofilho/test/MainActivity.kt
+++ app/src/main/java/com/germanofilho/test/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.google.gson.Gson
-import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,23 +18,61 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         val montadoraResponse = Gson().fromJson(getJsonFromAssets(), MontadoraAgregador::class.java)
+        val montadoraAux: ArrayList<Montadora> = ArrayList()
+        var contadorCarros = 0
+        var contadorPecas = 0
 
-        val montadoraAux: MutableList<Montadora> = ArrayList()
-
-        for (item in 0 until montadoraResponse.montadoras.size) {
-            if ("3".equals(montadoraResponse.montadoras[item].id)) {
-
-                montadoraAux.add(montadoraResponse.montadoras[item])
-//                montadoraResponse.montadoras.removeAt(item)
-//                montadoraResponse.montadoras[item] = montadoraResponse.montadoras[item+1]
-
-//                Log.d(TAG, "ta funcionando? ${montadoraResponse.montadoras[item]}")
+        montadoraResponse.montadoras?.let {listaMontadora ->
+            for (i in 0 until listaMontadora.size) {
+                listaMontadora[i].carros?.let {listaCarros ->
+                    if (listaCarros.isNotEmpty()) {
+                        for (j in 0 until listaCarros.size) {
+                            listaCarros[j].pecas?.let {
+                                if (listaCarros[j].pecas!!.isNotEmpty()) {
+                                    for (x in 0 until listaCarros[j].pecas!!.size) {
+                                        contadorPecas++
+                                    }
+                                    if (!listaCarros[j].nome.isNullOrEmpty()) {
+                                        Log.d(TAG, "A quantidade de peças do Carro: ${listaCarros[j].nome} = $contadorPecas")
+                                    } else {
+                                        Log.d(TAG, "A quantidade de peças em cada carro = $contadorPecas")
+                                    }
+                                } else {
+                                    Log.d(TAG, "A quantidade de peças em cada carro quando a lista de pecas for vazia : 0")
+                                }
+                                contadorPecas = 0
+                            }
+                            contadorCarros++
+                        }
+                        if (!listaMontadora[i].nome.isNullOrEmpty()) {
+                            Log.d(TAG, "A quantidade de carros da Montadora: ${listaMontadora[i].nome} = $contadorCarros")
+                        } else {
+                            Log.d(TAG, "A quantidade de carros em cada montadora = $contadorCarros")
+                        }
+                    }
+                }
+                contadorCarros = 0
             }
         }
 
-        Log.d(TAG, "ta funcionando? $montadoraAux")
+        montadoraResponse.montadoras?.let {listaMontadora ->
+            for (item in (listaMontadora.size - 1) downTo 1) {
+                if ("3".equals(listaMontadora[item].id)) {
+                    montadoraAux.add(listaMontadora[item])
+                    listaMontadora.removeAt(item)
+                }
+            }
+
+            for (i in 0 until montadoraAux.size) {
+                listaMontadora.add(montadoraAux[i])
+            }
+            Log.d(TAG, "Lista ordenada: $listaMontadora")
 
 
+            for (i in 0 until listaMontadora.size) {
+                Log.d(TAG, "Validando posicao $i: ${listaMontadora[i]}")
+            }
+        }
     }
 
     private fun getJsonFromAssets(): String {
--- app/src/main/java/com/germanofilho/test/model.kt
+++ app/src/main/java/com/germanofilho/test/model.kt
@@ -1,14 +1,14 @@
 package com.germanofilho.test
 
-data class MontadoraAgregador(val montadoras: MutableList<Montadora>)
+data class MontadoraAgregador(val montadoras: ArrayList<Montadora>?)
 
-data class Montadora(val id: String? = "0",
-                     val nome: String? = "0",
+data class Montadora(val id: String?,
+                     val nome: String?,
                      val carros: List<Carro>?)
 
 data class Carro(val id: String?,
                  val nome: String?,
-                 val pecas: List<Peca>)
+                 val pecas: List<Peca>?)
 
 data class Peca(val id: String?,
                 val nome: String?)
